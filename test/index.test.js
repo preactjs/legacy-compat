c98ff000a2dace14eb93e03b80a3ef9040d73288
Jason Miller <developit@users.noreply.github.com>
2021-05-12 15:54:49 -0400

    Add tests
    thanks @JoviDeCroock!

--- test/index.test.js
+++ test/index.test.js
@@ -45,21 +45,23 @@ describe('Component', () => {
 		});
 	});
 
-	describe('<img />', () => {
-		it('should remove px in width/height', () => {
-			ReactDOM.render(<img height="100px" width="200px" />, scratch);
-			expect(scratch.firstElementChild.getAttribute('height')).toEqual('100px');
-			expect(scratch.firstElementChild.getAttribute('width')).toEqual('200px');
-			// Note: can't use width/height properties, since they report incorrect values in JSDOM
-			//expect(scratch.firstElementChild.height).toEqual(100);
-			//expect(scratch.firstElementChild.width).toEqual(200);
-		});
+	describe('Attribute semantics for width & height props on media elements', () => {
+		describe.each(['img', 'video', 'canvas'])('%s', Tag => {
+			it('should remove px in width/height', () => {
+				ReactDOM.render(<Tag height="100px" width="200px" />, scratch);
+				expect(scratch.firstElementChild.getAttribute('height')).toEqual('100px');
+				expect(scratch.firstElementChild.getAttribute('width')).toEqual('200px');
+				// Note: can't use width/height properties, since they report incorrect values in JSDOM
+				//expect(scratch.firstElementChild.height).toEqual(100);
+				//expect(scratch.firstElementChild.width).toEqual(200);
+			});
 
-		it('should move % in width/height to style', () => {
-			ReactDOM.render(<img height="50%" width="100%" />, scratch);
-			expect(scratch.firstElementChild.getAttribute('height')).toEqual('50%');
-			expect(scratch.firstElementChild.getAttribute('width')).toEqual('100%');
-			// Note: can't use width/height properties, since they report incorrect values in JSDOM
+			it('should move % in width/height to style', () => {
+				ReactDOM.render(<Tag height="50%" width="100%" />, scratch);
+				expect(scratch.firstElementChild.getAttribute('height')).toEqual('50%');
+				expect(scratch.firstElementChild.getAttribute('width')).toEqual('100%');
+				// Note: can't use width/height properties, since they report incorrect values in JSDOM
+			});
 		});
 	});
 
